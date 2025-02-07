b7c5cb24bf1f7cee4e81757fa5c1efe6af7f29c8
Henok016 <henokmes1614@gmail.com>
2025-02-07 15:42:57 +0300

    Add files via upload
    added

---
+++ JavaFInal/src/Driver.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.sql.*;
+public class Driver extends Orders{
+    /*
+     * Driver instance variable declaration
+     */
+    private int driverID;
+    private String fName, lName, phoneNum;
+    private double salery;
+
+    public void getData(){
+        try{
+            jTextArea2.setText("");
+            Connection connection = SQLServerConnection.getConnection();
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM DRIVER;";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while(resultSet.next()){
+                driverID = resultSet.getInt("DriverID");
+                fName = resultSet.getString("F_Name");
+                lName = resultSet.getString("L_Name");
+                phoneNum = resultSet.getString("Phone");
+                salery = resultSet.getDouble("Salary");
+                jTextArea2.append(driverID + "\t         " + fName + "\t\t" + lName + "\t" + 
+                phoneNum + "\t\t " + salery + "\n");
+            }
+            SQLServerConnection.closeConnection();
+        }
+        catch(SQLException e){
+            ErrorDialog.showErrorDialog("Error in the database");
+            System.err.println("Error " + e);
+        }
+    }
+
+    public Driver() {
+        
+        jLabel1.setText("Driver ID");
+        
+        jLabel3.setText("First Name");
+        
+        jLabel4.setText("Last Name");
+        
+        jLabel5.setText("Phone Number");
+        
+        jLabel2.setText("Salery");
+        
+        jLabel6.setText(null);
+    }
+}
---
+++ JavaFInal/src/SQLServerConnection.java
@@ -0,0 +1,33 @@
+package src;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLServerConnection {
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=DMS3;encrypt=true;trustServerCertificate=true;integratedSecurity=true;";
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = getConnection()) {
+            if (connection != null) {
+                System.out.println("Connected to the database successfully!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
